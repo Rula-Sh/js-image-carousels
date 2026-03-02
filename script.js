76a3ca4662b464e02efcf9e612cb85422085ee7b
Rula-Sh <1.rulashaban@gmail.com>
2026-03-02 23:55:14 +0300

    Add a sliding carousel and update fade carousel structure

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-let images = document.querySelectorAll(".nature-image");
-let slideIndex = 0;
+let natureCarousel = document.querySelectorAll(".nature-image");
+let natureIndex = 0;
+natureCarousel[natureIndex].style.display = "block";
 
-images[slideIndex].style.display = "block";
+function moveNatureCarousel(indexUpdate) {
+  natureIndex += indexUpdate;
 
-function moveSlide(indexUpdate) {
-  slideIndex += indexUpdate;
-
-  images.forEach((image) => {
+  natureCarousel.forEach((image) => {
     image.style.display = "none";
   });
 
-  if (slideIndex === images.length) {
-    slideIndex = 0;
-  } else if (slideIndex < 0) {
-    slideIndex = images.length - 1;
+  if (natureIndex === natureCarousel.length) {
+    natureIndex = 0;
+  } else if (natureIndex < 0) {
+    natureIndex = natureCarousel.length - 1;
   }
 
-  images[slideIndex].style.display = "block";
+  natureCarousel[natureIndex].style.display = "block";
+}
+
+let animalCarousel = document.querySelectorAll(".animal-image");
+let animalIndex = 0;
+animalCarousel[animalIndex].style.display = "block";
+
+function moveAnimalsCarousel(indexUpdate) {
+  let oldIndex = animalIndex;
+  animalIndex += indexUpdate;
+
+  if (animalIndex === animalCarousel.length) {
+    animalIndex = 0;
+  } else if (animalIndex < 0) {
+    animalIndex = animalCarousel.length - 1;
+  }
+
+  if (indexUpdate === 1) {
+    animalCarousel[oldIndex].style.animation = "slide-to-left 1.5s";
+    setTimeout(() => {
+      animalCarousel[oldIndex].style.display = "none";
+    }, 1300);
+    animalCarousel[animalIndex].style.display = "block";
+    animalCarousel[animalIndex].style.animation = "slide-from-right 1.5s";
+  } else {
+    animalCarousel[oldIndex].style.animation = "slide-to-right 1.5s";
+    setTimeout(() => {
+      animalCarousel[oldIndex].style.display = "none";
+    }, 1300);
+    animalCarousel[animalIndex].style.animation = "slide-from-left 1.5s";
+    animalCarousel[animalIndex].style.display = "block";
+  }
 }
